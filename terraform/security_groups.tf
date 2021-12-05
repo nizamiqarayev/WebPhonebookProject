747969f97264c909f2a67dabda8333957e4ad338
nizamiqarayev <82280606+nizamiqarayev@users.noreply.github.com>
2021-12-05 22:43:09 +0400

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -61,8 +61,8 @@ resource "aws_security_group" "serversecurity" {
 
   egress {
       from_port = 0
-      to_port =  65535
-      protocol = "TCP"
+      to_port =  0
+      protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
     }
 }
